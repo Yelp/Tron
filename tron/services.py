75efd12af7ca3def2eebf62ac60d85d5b90718d7
Matthew Tytel <mtytel@thinktytel.(none)>
2010-09-24 21:32:51 -0700

    Incremental update for services rehaul

--- tron/services.py
+++ tron/services.py
@@ -4,13 +4,33 @@ class ServiceInstance(object):
     def __init__(self, service, inst_num):
         self.instance_num = inst_num
         self.service = service
+        self.pid_url = None
         self.state_callback = service.state_callback
         self.id = "%s.%s" % (service.name, self.instance_num)
 
         self.enabled = False
         self.node = None
         self.context = command_context.CommandContext(self, service.context)
+        
+        self.start_action = None
+        self.get_pid_action = Action("get_pid")
+        self.get_pid_action.command = "cat %(pid_url)s"
+        self.kill_action = Action("kill")
+        self.kill_action.command = "kill -1 %(pid)s"
 
+    def enable(self):
+        if not self.is_running():
+            self.start_action.next_run().start()
+
+    def disable(self):
+        if self.is_running():
+            self.kill_action.next_run().start()
+
+    def get_pid(self):
+        pass
+
+    def kill(self):
+        pass
 
 
 class Service(object):
@@ -23,5 +43,11 @@ class Service(object):
         self.count = None
         self.state_callback = lambda:None
         self.context = command_context.CommandContext(self)
+        self.instances = []
 
-    
+    def enable(self):
+        for i in self.instances:
+            i.enable()
+
+    def disable(self):
+        pass
