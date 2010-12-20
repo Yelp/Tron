044b63e0158564e844c8fb0293cbb26cf107adb0
Rhett Garber <rhettg@gmail.com>
2010-12-20 14:08:03 -0800

    Make fallback code actually work properly

--- tron/utils/twistedutils.py
+++ tron/utils/twistedutils.py
@@ -1,14 +1,19 @@
 from twisted.internet import defer, reactor
+from twisted.python import failure
 
 class Error(Exception): pass
+
+def _cancel(deferred):
+    """Re-implementing what's available in newer twisted in a crappy, but workable way."""
+    if not deferred.called:
+        deferred.errback(failure.Failure(Error()))
+    elif isinstance(deferred.result, defer.Deferred):
+        _cancel(deferred.result)
 
 def defer_timeout(deferred, timeout):
     try:
         reactor.callLater(timeout, deferred.cancel)
     except AttributeError:
-        # Re-implementing what's available in newer twisted in a crappy, but workable way.
-        if not deferred.called:
-            deferred.errback(failure.Failure(Error()))
-        elif isinstance(deferred.result, defer.Deferred):
-            defer_timeout(deferred.result, 0)
+        reactor.callLater(timeout, lambda : _cancel(deferred))
+
                 
