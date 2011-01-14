c10a41cc78e71fe8c1293c6d99ec914ab3ea7733
Rhett Garber <rhettg@gmail.com>
2011-01-14 13:12:21 -0800

    Version bump to 1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="tron",
-    version='0.1.9',
+    version='0.1.10',
     provides="tron",
     author="Yelp",
     author_email="yelplabs@yelp.com",
