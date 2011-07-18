c72460e8ad1b6d35a08bd3a3044f692b631f27db
Steve Johnson <sjohnson@yelp.com>
2011-07-18 13:09:45 -0700

    TronTestCase is able to upload and download config files. Other functionality is untested.

---
+++ test/trond_test.py
@@ -0,0 +1,34 @@
+from testify import *
+import yaml
+
+from test.trontestcase import TronTestCase
+
+
+BASIC_CONFIG = """
+--- !TronConfiguration
+ssh_options:
+        agent: true
+nodes:
+    - &local
+        hostname: 'localhost'
+"""
+
+SINGLE_ECHO_CONFIG = BASIC_CONFIG + """
+jobs:
+    - &echo_job
+        name: "echo_job"
+        node: *local
+        schedule: "interval 1 hour"
+        actions:
+            -
+                name: "echo_action"
+                command: "echo 'Echo!'"
+"""
+
+
+class BasicTronTestCase(TronTestCase):
+
+    def test_most_basic_thing_possible(self):
+        self.save_config(SINGLE_ECHO_CONFIG)
+        self.start_trond()
+        assert_equal(self.get_config(), SINGLE_ECHO_CONFIG)
--- test/trontestcase.py
+++ test/trontestcase.py
@@ -1,56 +1,131 @@
 import os
 import shutil
+import signal
+from subprocess import Popen, PIPE
 import tempfile
-from testify import TestCase
+from testify import *
+import time
+
+from tron import cmd
+
+
+# Used for getting the locations of the executables
+_test_folder, _ = os.path.split(__file__)
+_repo_root, _ = os.path.split(_test_folder)
+
+
+class TronSandboxException(Exception):
+    pass
+
+
+class MockConfigOptions(object):
+
+    def __init__(self, server):
+        self.server = server
+
 
 class TronTestCase(TestCase):
 
     @setup
     def make_sandbox(self):
-        self.tmp_dir = tempfile.mkdtemp()
-        test_folder, _ = os.path.split(__file__)
-        repo_root, _ = os.path.split(test_folder)
-        self.tron_bin = os.path.join(repo_root, 'bin')
+        """Set up a temp directory and storepaths to relevant binaries"""
+        # I had a really hard time not calling this function make_sandwich()
+        self.tmp_dir = tempfile.mkdtemp(prefix='tron-')
+        self.tron_bin = os.path.join(_repo_root, 'bin')
         self.tronctl_bin = os.path.join(self.tron_bin, 'tronctl')
         self.trond_bin = os.path.join(self.tron_bin, 'trond')
         self.tronfig_bin = os.path.join(self.tron_bin, 'tronfig')
         self.troview_bin = os.path.join(self.tron_bin, 'tronview')
 
+        self.log_file = os.path.join(self.tmp_dir, 'tron.log')
+        self.pid_file = os.path.join(self.tmp_dir, 'tron.pid')
+        self.config_file = os.path.join(self.tmp_dir, 'tron_config.yaml')
+
+        self.port = 8089
+        self.host = 'localhost'
+
         self.trond_debug_args = ['--working-dir=%s' % self.tmp_dir,
-                                 '--log-file=%s' % os.path.join(self.tmp_dir, 'tron.log'),
-                                 '--pid-file=%s' % os.path.join(self.tmp_dir, 'tron.pid')]
+                                 '--log-file=%s' % self.log_file,
+                                 '--pid-file=%s' % self.pid_file,
+                                 '--port=%d' % self.port,
+                                 '--host=%s' % self.host,
+                                 '-v', '-v', '-v']
+
+        self.tron_server_address = '%s:%d' % (self.host, self.port)
+        self.tron_server_uri = 'http://%s' % self.tron_server_address
+        self.tron_server_arg = '--server=%s' % self.tron_server_address
+
+        # mock a config object
+        self.config_obj = MockConfigOptions(self.tron_server_address)
+        cmd.save_config(self.config_obj)
+
+        self._last_trond_launch_args = []
 
     @teardown
     def delete_sandbox(self):
+        """Delete the temp directory and its contents"""
+        return
         shutil.rmtree(self.tmp_dir)
 
-    def start_tron(self, args=None):
-        args = args or None
-        # call trond
+    ### Configuration ###
 
-    def stop_tron(self):
-        pass
+    def save_config(self, config_text):
+        """Save a tron configuration to tron_config.yaml"""
+        with open(self.config_file, 'w') as f:
+            f.write(config_text)
+        return config_text
 
-    def restart_tron(self):
-        pass
+    def upload_config(self, config_text):
+        """Upload a tron configuration to the server"""
+        status, content = cmd.request(self.tron_server_uri,
+                                      'config',
+                                      {'config': config_text})
+        if 'error' in content:
+            raise TronSandboxException(content['error'])
+        else:
+            return status, content
 
-    def do_some_job_action(self):
-        pass
+    def get_config(self):
+        """Get the text of the current configuration"""
+        cmd.load_config(self.config_obj)
+        status, content = cmd.request(self.tron_server_uri, '/config')
+        if status != cmd.OK:
+            raise TronSandboxException(content)
+        else:
+            return content['config']
 
-    def tronctl(self):
-        pass
+    ### trond control ###
 
-    def tronview(self):
-        pass
+    def start_trond(self, args=None):
+        """Start trond"""
+        args = args or []
+        self._last_trond_launch_args = args
+        p = Popen([self.trond_bin] + self.trond_debug_args + args,
+                  stdout=PIPE, stderr=PIPE)
+        return p.communicate()
 
-    def exit_with_failure(self):
-        pass
+    def stop_trond(self):
+        """Stop trond based on the tron.pid in the temp directory"""
+        with open(self.pid_file, 'r') as f:
+            os.kill(int(f.read()), 0)
 
-    def exit_with_success(self):
-        pass
+    def restart_trond(self, args=None):
+        """Stop and start trond"""
+        if args == None:
+            args = self._last_trond_launch_args
+        self.stop_tron()
+        self.start_tron(args=args)
 
-    def daemonize(self):
-        pass
+    ### Basic subprocesses ###
 
-    def log_stream(self):
-        pass
+    def tronctl(self, args=None):
+        """Call tronctl with args"""
+        args = args or []
+        p = Popen([self.tronctl_bin] + args, stdout=PIPE, stderr=PIPE)
+        return p.communicate()
+
+    def tronview(self, args=None):
+        """Call tronview with args"""
+        args = args or []
+        p = Popen([self.tronview_bin] + args, stdout=PIPE, stderr=PIPE)
+        return p.communicate()
