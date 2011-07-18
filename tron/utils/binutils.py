46534a48de9d85604228f45408ed668ffa9da0cb
Steve Johnson <sjohnson@yelp.com>
2011-07-18 16:45:50 -0700

    Added docstrings to binutils

--- tron/utils/binutils.py
+++ tron/utils/binutils.py
@@ -1,10 +1,23 @@
+"""Functions either used by more than one executable or used by tests as well
+as executables
+"""
+
 def make_job_to_uri(content):
+    """Use ``content`` (the result of the '/' API call) to generate a dict
+    mapping job names to URIs
+    """
     return dict([(job['name'], job['href']) for job in content['jobs']])
 
 def make_service_to_uri(content):
+    """Use ``content`` (the result of the '/' API call) to generate a dict
+    mapping service names to URIs
+    """
     return dict([(service['name'], service['href']) for service in content['services']])
 
 def obj_spec_to_uri(obj_spec, job_to_uri, service_to_uri):
+    """Convert a string of the form job_name[.run_number[.action]] to its
+    corresponding URL
+    """
     obj_name_elements = obj_spec.split('.')
     obj_name = obj_name_elements[0]
     obj_rel_path = "/".join(obj_name_elements[1:])
