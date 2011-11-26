8bc11d63577bc4eb37c379e0180f285a76e613fb
Stephen Johnson <sjohnson@yelp.com>
2011-11-26 15:03:55 -0500

    Fix manifest, setup.cfg, docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,17 +13,14 @@
 
 import sys, os
 
-import tron
+sys.path.insert(0, os.path.abspath('..'))
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+import tron
 
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
