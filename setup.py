9f430a695a98e6ed546aa46b9618666f2d54e1a4
Stephen Johnson <sjohnson@yelp.com>
2011-11-28 18:40:17 -0500

    Fixed setup.py 'provides' and longdesc

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import tron
 setup(
     name="tron",
     version=tron.__version__,
-    provides="tron",
+    provides=["tron"],
     author="Yelp",
     author_email="yelplabs@yelp.com",
     url="http://github.com/Yelp/Tron",
@@ -39,5 +39,7 @@ setup(
             'docs/sample_config.yaml'
         ]),
     ],
-    long_description=open('README.md').read(),
+    long_description="""Tron is a centralized system for managing periodic batch processes and services across a cluster. If you find cron or fcron to be insufficient for managing complex work flows across multiple computers, Tron might be for you.
+
+For more information, look at the tutorial (http://packages.python.org/tron/tutorial.html) or the full documentation (http://packages.python.org/tron).""",
 )
