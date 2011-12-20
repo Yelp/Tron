e3f1c699533fc3bc296f4915b6798bb3e23470bc
Stephen Johnson <sjohnson@yelp.com>
2011-12-20 15:43:47 -0500

    pep8ify tron.utils

--- tron/utils/log_handler.py
+++ tron/utils/log_handler.py
@@ -1,8 +1,12 @@
-"""The default logging module doesn't provide a way to re-open log files based on SIGHUP"""
+"""The default logging module doesn't provide a way to re-open log files based
+on SIGHUP
+"""
 
 import logging
 
+
 class ReOpeningFileHandler(logging.FileHandler):
+
     def __init__(self, filename, mode='a', encoding=None):
         self.filename = filename
         self.mode = mode
@@ -10,18 +14,16 @@ class ReOpeningFileHandler(logging.FileHandler):
         self.open_stream()
 
         logging.StreamHandler.__init__(self, self.stream)
-    
+
     def open_stream(self):
         assert self.stream is None
         self.stream = open(self.filename, self.mode)
-    
+
     def close_stream(self):
         self.flush()
         self.stream.close()
         self.stream = None
-    
+
     def reopen(self):
         self.close_stream()
         self.open_stream()
-        
-    
--- tron/utils/timeutils.py
+++ tron/utils/timeutils.py
@@ -2,16 +2,19 @@
 import datetime
 import time
 
+
 # Global time override
 _current_time_override = None
 
+
 def override_current_time(current_time):
     """Interface for overriding the current time
-    
+
     This is a test hook for forcing the app to think it's a specific time.
     """
     global _current_time_override
     _current_time_override = current_time
+
 
 def current_time():
     """Standard interface for generating the current time."""
@@ -20,12 +23,15 @@ def current_time():
     else:
         return datetime.datetime.now()
 
+
 def current_timestamp():
     return to_timestamp(current_time())
-        
+
+
 def to_timestamp(time_val):
     """Generate a unix timestamp for the given datetime instance"""
     return time.mktime(time_val.timetuple())
+
 
 def macro_timedelta(start_date, years=0, months=0, days=0):
     """Since datetime doesn't provide timedeltas at the year or month level,
@@ -41,6 +47,8 @@ def macro_timedelta(start_date, years=0, months=0, days=0):
         new_month += 12
         years -= 1
 
-    delta += datetime.datetime(start_date.year+years, new_month, start_date.day) - start_date
+    end_date = datetime.datetime(start_date.year + years,
+                                 new_month, start_date.day)
+    delta += end_date - start_date
 
     return delta
