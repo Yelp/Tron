29366f7fcf06c742aa5d057e3055c1fec818b12e
Daniel Nephin <dnephin@yelp.com>
2013-02-12 08:23:17 -0800

    Bump version to 0.5.2.1 and add release notes.

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,4 +1,4 @@
-__version_info__    = (0, 5, 2, 0)
+__version_info__    = (0, 5, 2, 1)
 __version__         = ".".join("%s" % v for v in __version_info__)
 
 __author__          = 'Yelp <yelplabs@yelp.com>'
