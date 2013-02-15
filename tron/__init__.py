55fafde9e5247076d26c4b1b8576e17c45674fea
Daniel Nephin <dnephin@yelp.com>
2013-02-15 11:21:27 -0800

    Bump version.

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,4 +1,4 @@
-__version_info__    = (0, 5, 2, 2)
+__version_info__    = (0, 5, 2, 3)
 __version__         = ".".join("%s" % v for v in __version_info__)
 
 __author__          = 'Yelp <yelplabs@yelp.com>'
