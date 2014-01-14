05e2e06a3c9fb9c4443e828eba5490dbac1c89c2
Yejun Yang <yejun@yelp.com>
2014-01-14 10:39:26 -0800

    bump version to 6.1.2

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,4 +1,4 @@
-__version_info__    = (0, 6, 1, 1)
+__version_info__    = (0, 6, 1, 2)
 __version__         = ".".join("%s" % v for v in __version_info__)
 
 __author__          = 'Yelp <yelplabs@yelp.com>'
