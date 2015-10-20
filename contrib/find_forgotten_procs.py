bff48cdc55013e231a78332b5a4af4ca0e87b011
Paulo Koch <pkoch@yelp.com>
2015-10-20 13:54:57 -0700

    Don't require fabric

--- contrib/find_forgotten_procs.py
+++ contrib/find_forgotten_procs.py
@@ -3,39 +3,19 @@ Find processes missing from Tron.
 """
 import json
 import optparse
-import sys
+import subprocess
 import urllib2
+import sys
 from fnmatch import fnmatch
+from multiprocessing.pool import ThreadPool
 from pprint import pprint
+from tempfile import TemporaryFile
 
-try:
-    from fabric.version import VERSION, get_version
-    if VERSION < (1, 3):
-        raise Exception(
-            "This script needs fabric>=1.3 installed. You have {0}".format(
-                get_version()
-            )
-        )
-    del VERSION, get_version
-except ImportError:
-    print >>sys.stderr, \
-        "Can't import fabric. Please make sure it's in your sys.path"
-    raise
-except:
-    raise Exception(
-        "Can't detect fabric's version. This script needs fabric>=1.3. Make "
-        "sure it's installed and in your sys.path"
-    )
-
-from fabric.api import run, settings, execute, parallel
-from fabric.context_managers import hide
-from fabric import state
-
-state.env.forward_agent = True
 
 
 DEFAULT_SIGNAL = 'TERM'
 DEFAULT_KILL_PREFIX = ''
+DEFAULT_MAX_THREADS = 64
 
 
 COMMANDS = (
@@ -44,15 +24,27 @@ COMMANDS = (
 )
 
 
-def clean_output(o, ignore_outputs_starting_with):
-    if not ignore_outputs_starting_with:
-        return o
+def _check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
 
-    return '\n'.join([
-        l.strip()
-        for l in o.split('\n')
-        if not l.startswith(ignore_outputs_starting_with)
-    ])
+    if 'stdin' in kwargs:
+        curr = kwargs['stdin']
+        if isinstance(curr, basestring):
+            f = TemporaryFile()
+            f.write(curr)
+            f.seek(0)
+            kwargs['stdin'] = f
+
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
 
 
 def pidfiles_for_host(hostname, services):
@@ -76,90 +68,105 @@ def pidfiles_for_host(hostname, services):
     ]
 
 
-def get_instances(search, ignore_outputs_starting_with=None):
-    with settings(warn_only=True):
-        with hide('stdout', 'stderr', 'warnings'):
-            out = clean_output(
-                run('pgrep -f %s' % search),
-                ignore_outputs_starting_with,
-            )
-        pids = filter(
-            bool,
-            (
-                l.strip()
-                for l in out.replace('\r', '').split('\n')
-            ),
-        )
-        if not pids:
-            return []
+def ssh_get_instances(host, service_to_target):
+    remote_output = _check_output(
+        ['ssh', host, 'bash'],
+        stdin="""
+            OUT_LOCK=$(mktemp)
+            declare -A services=( %(bash_hash)s )
+            for service in "${!services[@]}"
+            do
+                (
+                    RESULT=$(
+                        echo $service
+                        pgrep -f "${services["$service"]}" |
+                            grep '^[0-9]\+' |
+                            xargs -r ps --no-headers -o pid,command -p
+                    )
+                    flock $OUT_LOCK -c "echo "'"'"$RESULT"'"'"; echo"
+                )&
+            done
+            wait
+            rm $OUT_LOCK
+        """ % {
+            'bash_hash': ' '.join([
+                '["%s"]="%s"' % (s, t,)
+                for s, t in service_to_target.iteritems()
+            ])
+        },
+    )
 
-        with hide('running', 'stdout', 'stderr', 'warnings'):
-            out = clean_output(
-                run('ps -o pid,command -p %s | cat' % ' '.join(pids)),
-                ignore_outputs_starting_with,
-            )
-        result = [
-            l.split(None, 1)
-            for l in out.split('\n')[1:]
+    found_processes = {}
+    for chunk in remote_output.split('\n\n'):
+        lines = chunk.split('\n')
+        service_name = lines[0]
+        rest = lines[1:]
+
+        found_processes[service_name] = [
+           line.split(None, 1)
+           for line in rest
         ]
-        return result
-    return result
 
-def find_forgotten(services, ignore_outputs_starting_with=None):
-    with hide('running', 'stdout', 'stderr', 'warnings'):
-        my_hostname = clean_output(
-            run('hostname'),
-            ignore_outputs_starting_with,
-        ).strip()
+    return found_processes
 
-    lost_pids = {}
 
+def find_forgotten(host, services):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    service_names = []
+    service_to_target = {}
+    serivce_to_expecteds = {}
     for (
         service_name,
         globbable_pidfile,
         expected_pidfiles,
-    ) in pidfiles_for_host(my_hostname, services):
-        unknowns = [
-            pid
-            for pid, command in get_instances(
-                globbable_pidfile,
-                ignore_outputs_starting_with,
-            )
-            if not any(pidfile in command for pidfile in expected_pidfiles)
-        ]
-        if unknowns:
-            lost_pids[service_name] = unknowns
+    ) in pidfiles_for_host(host, services):
+        service_names.append(service_name)
+        service_to_target[service_name] = globbable_pidfile
+        serivce_to_expecteds[service_name] = expected_pidfiles
 
+    lost_pids = {}
+    for service_name, pid_command_pairs in ssh_get_instances(
+        host,
+        service_to_target,
+    ).iteritems():
+        lost = [
+            pid
+            for pid, command in pid_command_pairs
+            if not any(
+                expected_pidfile in command
+                for expected_pidfile in serivce_to_expecteds[service_name]
+            )
+        ]
+
+        if lost:
+            lost_pids[service_name] = lost
+
+    sys.stdout.write('\b \b')
+    sys.stdout.flush()
     return lost_pids.items()
 
 
 def kill_forgotten(
+    host,
     results,
     signal=DEFAULT_SIGNAL,
     kill_prefix=DEFAULT_KILL_PREFIX,
-    ignore_outputs_starting_with=None,
 ):
-    with hide('running', 'stdout', 'stderr', 'warnings'):
-        my_hostname = clean_output(
-            run('hostname'),
-            ignore_outputs_starting_with,
-        ).strip()
-
-    my_targets = sum((pids for _, pids in results[my_hostname]), [])
+    my_targets = sum((pids for _, pids in results[host]), [])
 
     if not my_targets:
         return
 
-    with settings(warn_only=True):
-        clean_output(
-            run(
-                ' '.join([kill_prefix, 'kill -s {0} {1}']).strip().format(
-                    signal,
-                    ' '.join(my_targets),
-                ),
-            ),
-            ignore_outputs_starting_with,
-        )
+    return _check_output(
+        ['ssh', '-A', host, 'bash'],
+        stdin=' '.join(
+            [kill_prefix, 'kill -s {0} {1} || true'],
+        ).strip().format(
+            signal,
+            ' '.join(my_targets),
+        ),
+    )
 
 
 def _get_services_from_tron(tron_base):
@@ -174,15 +181,17 @@ def _get_services_from_tron(tron_base):
 def main(command, tron_base, *target_service_names, **options):
     options.setdefault('signal', DEFAULT_SIGNAL)
     options.setdefault('kill_prefix', DEFAULT_KILL_PREFIX)
-    options.setdefault('ignore_outputs_starting_with', None)
+    options.setdefault('max_threads', DEFAULT_MAX_THREADS)
 
+    print "Getting services"
     services = _get_services_from_tron(tron_base)
-    all_nodes = set([
+    print "done!"
+
+    all_hosts = set([
         node['hostname']
         for service in services
         for node in service['node_pool']['nodes']
     ])
-
     if target_service_names:
         target_services = [
             service
@@ -191,12 +200,13 @@ def main(command, tron_base, *target_service_names, **options):
         ]
     else:
         target_services = services
+    pool = ThreadPool(min(len(all_hosts), options['max_threads']))
 
-    results = execute(
-        parallel(find_forgotten),
-        target_services,
-        options['ignore_outputs_starting_with'],
-        hosts=all_nodes,
+    results = dict(
+        pool.imap_unordered(
+            lambda host: (host, find_forgotten(host, target_services),),
+            all_hosts,
+        ),
     )
 
     pprint(results)
@@ -208,29 +218,36 @@ def main(command, tron_base, *target_service_names, **options):
     print "Total instances", forgotten_count
 
     if forgotten_count and command == 'kill':
-        execute(
-            parallel(kill_forgotten),
-            results,
-            options['signal'],
-            options['kill_prefix'],
-            options['ignore_outputs_starting_with'],
-            hosts=set([
+        for host, output in pool.imap_unordered(
+            lambda host: (
+                host,
+                    kill_forgotten(
+                    host,
+                    results,
+                    options['signal'],
+                    options['kill_prefix'],
+                ),
+            ),
+            set([
                 hostname
                 for hostname, pairs in results.iteritems()
                 if pairs
             ]),
-        )
+        ):
+            if not output:
+                continue
+            print host, 'said: ', output
 
 def _make_opts():
     parser = optparse.OptionParser(
         usage='usage: %prog [options] <command> <tron_base> <target_service_name>*'
     )
     parser.add_option(
-        '--ignore-outputs-starting-with',
-        default=None,
-        help="Ignore these lines in the stdout of the invoked program. This "
-            "is useful when you have something that behaves funkily with "
-            "fabirc in your profile/bashrc/etc.",
+        '-j',
+        '--max-threads',
+        default=DEFAULT_MAX_THREADS,
+        type=int,
+        help="Max number of local threads to use to span ssh connections.",
     )
     kill_group = optparse.OptionGroup(parser, 'kill options')
     kill_group.add_option(
