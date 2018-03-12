1f3751c5a3d14ebc04093d9c4c0f66044c22b705
Joseph Lukasik <jgl@yelp.com>
2018-03-12 15:28:54 -0700

    Allow tab complete to read from cached file
    If tab complete cache file does not exist, it will fallback to hitting api
    Also adds the debian links file to automatically enable tab completion

---
+++ bin/tron_tabcomplete.sh
@@ -0,0 +1,8 @@
+if [[ -n ${ZSH_VERSION-} ]]; then
+	autoload -U +X bashcompinit && bashcompinit
+fi
+
+# This magic eval enables tab-completion for tron commands
+# http://argcomplete.readthedocs.io/en/latest/index.html#synopsis
+eval "$(/opt/venvs/tron/bin/register-python-argcomplete tronview)"
+eval "$(/opt/venvs/tron/bin/register-python-argcomplete tronctl)"
