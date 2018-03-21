95557f13bba2658d013a4c5ffaf644cf2c95f1b9
maksym <maksym@yelp.com>
2018-03-21 14:09:02 +0100

    implement and use backward compatible Py2Shelf

--- tron/serialize/runstate/shelvestore.py
+++ tron/serialize/runstate/shelvestore.py
@@ -1,15 +1,55 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import dbm
 import logging
 import operator
+import pickle
 import shelve
+import sys
+from io import BytesIO
 
 from six.moves import filter
 from six.moves import zip
 
 
 log = logging.getLogger(__name__)
+
+
+class Py2Shelf(shelve.Shelf):
+    def __init__(self, filename, flag='c', protocol=2, writeback=False, keyencoding='ascii'):
+        self.keyencoding = keyencoding
+
+        if sys.version_info[0] == 3:
+            shelve.Shelf.__init__(
+                self, dbm.open(
+                    filename, flag,
+                ), protocol, writeback, keyencoding,
+            )
+        else:
+            shelve.Shelf.__init__(
+                self, dbm.open(filename, flag), protocol, writeback,
+            )
+
+    def __getitem__(self, key):
+        try:
+            value = self.cache[key]
+        except KeyError:
+            f = BytesIO(self.dict[key.encode(self.keyencoding)])
+            if sys.version_info[0] == 3:
+                value = pickle.load(f, encoding='bytes')
+            else:
+                value = pickle.load(f)
+            if self.writeback:
+                self.cache[key] = value
+        return value
+
+    def __setitem__(self, key, value):
+        if self.writeback:
+            self.cache[key] = value
+        f = BytesIO()
+        pickle.dump(obj=value, file=f, protocol=self._protocol)
+        self.dict[key.encode(self.keyencoding)] = f.getvalue()
 
 
 class ShelveKey(object):
@@ -38,7 +78,10 @@ class ShelveStateStore(object):
 
     def __init__(self, filename):
         self.filename = filename
-        self.shelve = shelve.open(self.filename)
+        self.shelve = Py2Shelf(
+            self.filename,
+            keyencoding='utf8',
+        )
 
     def build_key(self, type, iden):
         return ShelveKey(type, iden)
