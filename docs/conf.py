e808d3a907a96b5567564066aec285bf74726348
Kyle Anderson <kwa@yelp.com>
2018-03-29 14:29:07 -0700

    Mock out bsddb3 in doc generation

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,19 @@ from __future__ import unicode_literals
 
 import os
 import sys
+from unittest.mock import MagicMock
 
 sys.path.insert(0, os.path.abspath('..'))
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['bsddb3']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- General configuration -----------------------------------------------------
