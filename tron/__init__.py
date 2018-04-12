94b540fd59e9c8e4b47bc59c98083f5203d8509e
maksym <maksym@yelp.com>
2018-04-12 15:30:07 +0200

    Released 0.7.8.3 via make release

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version_info__ = (0, 7, 8, 2)
+__version_info__ = (0, 7, 8, 3)
 __version__ = ".".join("%s" % v for v in __version_info__)
 __author__ = 'Yelp <yelplabs@yelp.com>'
 __credits__ = [
