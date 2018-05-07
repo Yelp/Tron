b2e8ae956421d6937981844d4ca73843662bf8f1
maksym <maksym@yelp.com>
2018-05-07 22:03:44 +0100

    basic navigation and fetch with lock

--- tronweb2/src/tronweb2/core.cljs
+++ tronweb2/src/tronweb2/core.cljs
@@ -4,26 +4,39 @@
 
 (def app-state (reagent/atom {}))
 
+(defn jobs [state]
+  [:div.container
+    (for [j (:jobs state)]
+      [:div.card.w-25 {:key (j "name")}
+        [:div.card-body
+          [:h6.card-title (str (j "name"))]
+          [:p.card-text
+            "qwrf we gert gqe rf wertg ert gq er fw treg w erf qwer f"]]])])
+
+(defn configs [state]
+  [:div.container "Configs"])
+
+(def views
+  {:jobs jobs
+   :configs configs})
+
 (defn root []
   [:div.container-fluid
     [:nav.navbar.navbar-expand-lg.navbar-light.bg-light
       [:a.navbar-brand {:href "#/"} "Tron"]
-
       [:ul.navbar-nav.mr-auto
-        [:li.nav-item.active
-          [:a.nav-link {:href "#/"} "Jobs" [:span.sr-only "(current)"]]]]
+        [:li.nav-item
+          [:a.nav-link {:href "#/"} "Jobs"]]
+        [:li.nav-item
+          [:a.nav-link {:href "#/configs"} "Configs"]]]
       [:form.form-inline.my-2.my-lg-0
         [:input.form-control.mr-sm-2 {:type "search" :placeholder "Search"}]
         [:button.btn.btn-outline-success.my-2.my-sm-0 {:type "submit"} "Search"]]]
     [:br]
-    (let [state @app-state]
-      [:div.container
-        (for [j (:jobs state)]
-          [:div.card.w-25 {:key (j "name")}
-            [:div.card-body
-              [:h6.card-title (str (j "name"))]
-              [:p.card-text
-                "qwrf we gert gqe rf wertg ert gq er fw treg w erf qwer f"]]])])])
+    (let [state @app-state
+          view (:view state)]
+      ((views view) state))])
+
 
 (defn ^:export init []
   (enable-console-print!)
--- tronweb2/src/tronweb2/routes.cljs
+++ tronweb2/src/tronweb2/routes.cljs
@@ -14,14 +14,41 @@
        (secretary/dispatch! (.-token event))))
     (.setEnabled true)))
 
+(defn fetch-jobs! [state]
+  (when-not (:jobs-inflight @state)
+    (swap! state assoc :jobs-inflight true)
+    (GET "/api/jobs"
+      :params {:include_job_runs 1}
+      :handler
+      #(swap! state merge {:jobs (% "jobs")
+                           :jobs-inflight false
+                           :error-message nil})
+      :error-handler
+      #(swap! state merge {:error-message "Failed to load jobs"
+                           :jobs-inflight false}))))
+
+(defn fetch-api! [state]
+  (when-not (:api-inflight @state)
+    (swap! state assoc :api-inflight true)
+    (GET "/api"
+      :handler
+      #(swap! state merge {:api (% "api")
+                           :api-inflight false
+                           :error-message nil})
+      :error-handler
+      #(swap! state merge {:error-message "Failed to load api"
+                           :api-inflight false}))))
+
+
 (defn setup [state]
   (secretary/set-config! :prefix "#")
 
   (defroute "/" []
-    (.log js/console "nav to /")
-    (swap! state assoc :view :jobs)
-    (GET "/api/jobs"
-      :params {:include_job_runs 1}
-      :handler #(swap! state assoc :jobs (% "jobs"))))
+    (swap! state merge {:view :jobs})
+    (fetch-jobs! state))
+
+  (defroute "/configs" []
+    (swap! state merge {:view :configs})
+    (fetch-api! state))
 
   (hook-browser-navigation!))
