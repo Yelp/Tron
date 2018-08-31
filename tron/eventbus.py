b0cf32aec4f1a79a513020f07ae10693abdbe73f
maksym <maksym@yelp.com>
2018-08-31 14:28:13 +0200

    dont crash when saving eventbus log into file that already exists

--- tron/eventbus.py
+++ tron/eventbus.py
@@ -80,8 +80,16 @@ class EventBus:
     def sync_save_log(self, reason):
         started = time.time()
         new_file = os.path.join(self.log_dir, f"{int(started)}.pickle")
-        with open(new_file, 'xb') as f:
-            pickle.dump(self.event_log, f)
+        try:
+            with open(new_file, 'xb') as f:
+                pickle.dump(self.event_log, f)
+        except FileExistsError:
+            log.exception(
+                f"unable to dump the log, file {new_file} already exists, "
+                f"too many updates/sec? current: {self.log_updates}, "
+                f"threshold: {self.log_save_updates}"
+            )
+            return False
 
         tmplink = os.path.join(self.log_dir, "tmp")
         os.symlink(new_file, tmplink)
@@ -89,6 +97,7 @@ class EventBus:
 
         duration = time.time() - started
         log.info(f"log dumped to disk because {reason}, took {duration:.4}s")
+        return True
 
     def sync_loop(self):
         if not self.enabled:
@@ -116,8 +125,7 @@ class EventBus:
         elif self.log_updates > self.log_save_updates:
             save_reason = f"{self.log_save_updates} updates"
 
-        if save_reason:
-            self.sync_save_log(save_reason)
+        if save_reason and self.sync_save_log(save_reason):
             self.log_last_save = time.time()
             self.log_updates = 0
 
