710d25988f1aa9ca8226f0a9047ab0a6670e1619
Ka Wai Wan <kawaiwan@yelp.com>
2018-11-20 14:00:10 -0800

    Bump to 0.9.9.4 in __init__.py

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version_info__ = (0, 9, 9, 3)
+__version_info__ = (0, 9, 9, 4)
 __version__ = ".".join("%s" % v for v in __version_info__)
 __author__ = 'Yelp <yelplabs@yelp.com>'
 __credits__ = [
