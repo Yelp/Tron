8e1e176ae01ae47ca162cfedbab76300baf18f88
maksym <maksym@yelp.com>
2019-05-14 16:28:08 +0300

    use csafedumper/loader

--- tron/yaml.py
+++ tron/yaml.py
@@ -1,20 +1,21 @@
 import yaml
 
-safe_dump = yaml.safe_dump
-safe_load = yaml.safe_load
-safe_load_all = yaml.safe_load_all
-
 
 def dump(*args, **kwargs):
-    kwargs['Dumper'] = yaml.CDumper
-    return yaml.safe_dump(*args, **kwargs)
+    kwargs['Dumper'] = yaml.CSafeDumper
+    return yaml.dump(*args, **kwargs)
 
 
 def load(*args, **kwargs):
-    kwargs['Loader'] = yaml.CLoader
-    return yaml.safe_load(*args, **kwargs)
+    kwargs['Loader'] = yaml.CSafeLoader
+    return yaml.load(*args, **kwargs)
 
 
 def load_all(*args, **kwargs):
-    kwargs['Loader'] = yaml.CLoader
-    return yaml.safe_load_all(*args, **kwargs)
+    kwargs['Loader'] = yaml.CSafeLoader
+    return yaml.load_all(*args, **kwargs)
+
+
+safe_dump = dump
+safe_load = load
+safe_load_all = load_all
