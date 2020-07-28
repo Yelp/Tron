90c25bafe5611d8d227e7987338c653bfc778649
Qui Nguyen <1322636+qui@users.noreply.github.com>
2020-07-28 15:24:36 -0700

    Released 1.4.0 via make release

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version_info__ = (1, 3, 15)
+__version_info__ = (1, 4, 0)
 __version__ = ".".join("%s" % v for v in __version_info__)
 __author__ = 'Yelp <yelplabs@yelp.com>'
 __credits__ = [
