2c7c7831c3b16d0c46d8fd3f6dee64e34b2ea392
Qui Nguyen <1322636+qui@users.noreply.github.com>
2020-08-26 16:56:29 -0700

    Don't crash if we can't restore a single run

--- tests/serialize/runstate/statemanager_test.py
+++ tests/serialize/runstate/statemanager_test.py
@@ -153,6 +153,20 @@ class TestPersistentStateManager(TestCase):
             ]
             assert runs == ['two', 'three']
 
+    def test_restore_runs_for_job_one_missing(self):
+        job_state = {'run_nums': [2, 3], 'enabled': True}
+        with mock.patch.object(
+            self.manager, '_restore_dicts', autospec=True,
+        ) as mock_restore_dicts:
+            mock_restore_dicts.side_effect = [{}, {'job_a.3': 'three'}]
+            runs = self.manager._restore_runs_for_job('job_a', job_state)
+
+            assert mock_restore_dicts.call_args_list == [
+                mock.call(runstate.JOB_RUN_STATE, ['job_a.2']),
+                mock.call(runstate.JOB_RUN_STATE, ['job_a.3']),
+            ]
+            assert runs == ['three']
+
     def test_restore_dicts(self):
         names = ['namea', 'nameb']
         autospec_method(self.manager._keys_for_items)
