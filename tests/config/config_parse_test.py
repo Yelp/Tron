6dadc8ac9ff3394291c8f0053dbe88301653ec2e
Luis Perez <luisp@yelp.com>
2021-05-24 13:03:28 -0700

    Add initial k8s config + global k8s toggle + job opt-out
    We'll be adding two toggles to control Tron's usage of k8s:
    * a global killswitch (k8s_options["enabled"]) for when we want to go
      back to Mesos for an entire cluster (and, in the future, for when we
      want to quickly stop all Tronjobs).
    * a per-job opt-out (`use_k8s`) for any jobs that encounter issues with
      k8s or that we want to migrate at a specific time.
    
    Since I was adding a k8s config section to the Tron master config, I
    also went ahead and added a way to configure what the k8s API address
    should be (i.e., the "master" address).

--- tests/config/config_parse_test.py
+++ tests/config/config_parse_test.py
@@ -163,6 +163,7 @@ def make_job(**kwargs):
     kwargs.setdefault('allow_overlap', False)
     kwargs.setdefault('time_zone', None)
     kwargs.setdefault('expected_runtime', datetime.timedelta(0, 3600))
+    kwargs.setdefault('use_k8s', True)
     return schema.ConfigJob(**kwargs)
 
 
