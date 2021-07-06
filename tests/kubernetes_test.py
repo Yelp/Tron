c58dc271b00aa22374a1b042aca9cea5df91fe84
Luis Pérez <luisp@yelp.com>
2021-07-06 15:28:53 -0400

    Scaffold out KubernetesCluster (+ some initial implementation) (#814)
    This sets up the skeleton for KubernetesCluster (a k8s version of
    MesosCluster) and does some of the basic implementation for several
    methods when possible/easy

--- tests/kubernetes_test.py
+++ tests/kubernetes_test.py
@@ -6,6 +6,7 @@ import pytest
 from task_processing.interfaces.event import Event
 from task_processing.plugins.kubernetes.task_config import KubernetesTaskConfig
 
+from tron.kubernetes import KubernetesCluster
 from tron.kubernetes import KubernetesTask
 
 
@@ -18,6 +19,22 @@ def mock_kubernetes_task():
             action_run_id="mock_service.mock_job.1.mock_action",
             task_config=KubernetesTaskConfig(name="mock--service-mock-job-mock--action", uuid="123456",),
         )
+
+
+@pytest.fixture
+def mock_kubernetes_cluster():
+    with mock.patch("tron.kubernetes.PyDeferredQueue", autospec=True,), mock.patch(
+        "tron.kubernetes.TaskProcessor", autospec=True,
+    ):
+        yield KubernetesCluster("kube-cluster-a:1234")
+
+
+@pytest.fixture
+def mock_disabled_kubernetes_cluster():
+    with mock.patch("tron.kubernetes.PyDeferredQueue", autospec=True,), mock.patch(
+        "tron.kubernetes.TaskProcessor", autospec=True,
+    ):
+        yield KubernetesCluster("kube-cluster-a:1234", enabled=False)
 
 
 def mock_event_factory(
@@ -73,3 +90,118 @@ def test_handle_event_exit_early_on_misrouted_event(mock_kubernetes_task):
     # we log before actually doing anything with an event, so this not being called means
     # we exited early
     assert not mock_log_event_info.called
+
+
+def test_create_task_disabled():
+    cluster = KubernetesCluster("kube-cluster-a:1234", enabled=False)
+    mock_serializer = mock.MagicMock()
+
+    task = cluster.create_task(action_run_id="action_a", serializer=mock_serializer,)
+
+    assert task is None
+
+
+def test_create_task(mock_kubernetes_cluster):
+    mock_serializer = mock.MagicMock()
+
+    task = mock_kubernetes_cluster.create_task(action_run_id="action_a", serializer=mock_serializer,)
+
+    assert task is not None
+
+
+def test_create_task_with_task_id(mock_kubernetes_cluster):
+    mock_serializer = mock.MagicMock()
+
+    task = mock_kubernetes_cluster.create_task(action_run_id="action_a", serializer=mock_serializer, task_id="yay.1234")
+
+    mock_kubernetes_cluster.runner.TASK_CONFIG_INTERFACE().set_pod_name.assert_called_once_with("yay.1234")
+    assert task is not None
+
+
+def test_create_task_with_invalid_task_id(mock_kubernetes_cluster):
+    mock_serializer = mock.MagicMock()
+
+    with mock.patch.object(mock_kubernetes_cluster, "runner") as mock_runner:
+        mock_runner.TASK_CONFIG_INTERFACE.return_value.set_pod_name = mock.MagicMock(side_effect=ValueError)
+        task = mock_kubernetes_cluster.create_task(action_run_id="action_a", serializer=mock_serializer, task_id="boo")
+
+    assert task is None
+
+
+def test_process_event_task(mock_kubernetes_cluster):
+    event = mock_event_factory(task_id="abc.123", platform_type="mock_type")
+    mock_kubernetes_task = mock.MagicMock(spec_set=KubernetesTask)
+    mock_kubernetes_task.get_kubernetes_id.return_value = "abc.123"
+    mock_kubernetes_cluster.tasks["abc.123"] = mock_kubernetes_task
+
+    mock_kubernetes_cluster.process_event(event)
+
+    mock_kubernetes_task.handle_event.assert_called_once_with(event)
+
+
+def test_process_event_task_invalid_id(mock_kubernetes_cluster):
+    event = mock_event_factory(task_id="hwat.dis", platform_type="mock_type")
+    mock_kubernetes_task = mock.MagicMock(spec_set=KubernetesTask)
+    mock_kubernetes_task.get_kubernetes_id.return_value = "abc.123"
+    mock_kubernetes_cluster.tasks["abc.123"] = mock_kubernetes_task
+
+    mock_kubernetes_cluster.process_event(event)
+
+    assert mock_kubernetes_task.handle_event.call_count == 0
+
+
+def test_stop_default(mock_kubernetes_cluster):
+    # When stopping, tasks should not exit. They will be recovered
+    mock_task = mock.MagicMock()
+    mock_kubernetes_cluster.tasks = {"task_id": mock_task}
+    mock_kubernetes_cluster.stop()
+    assert mock_kubernetes_cluster.deferred is None
+    assert mock_task.exited.call_count == 0
+    assert len(mock_kubernetes_cluster.tasks) == 1
+
+
+def test_stop_disabled():
+    # Shouldn't raise an error
+    mock_kubernetes_cluster = KubernetesCluster("kube-cluster-a:1234", enabled=False)
+    mock_kubernetes_cluster.stop()
+
+
+def test_set_enabled_enable_already_on(mock_kubernetes_cluster):
+    mock_kubernetes_cluster.set_enabled(is_enabled=True)
+
+    assert mock_kubernetes_cluster.enabled is True
+    # only called once as part of creating the cluster object
+    mock_kubernetes_cluster.processor.executor_from_config.assert_called_once()
+    assert mock_kubernetes_cluster.runner is not None
+    assert mock_kubernetes_cluster.deferred is not None
+    mock_kubernetes_cluster.deferred.addCallback.assert_has_calls(
+        [mock.call(mock_kubernetes_cluster.process_event), mock.call(mock_kubernetes_cluster.handle_next_event),]
+    )
+
+
+def test_set_enabled_enable(mock_disabled_kubernetes_cluster):
+    mock_disabled_kubernetes_cluster.set_enabled(is_enabled=True)
+
+    assert mock_disabled_kubernetes_cluster.enabled is True
+    # only called once as part of enabling
+    mock_disabled_kubernetes_cluster.processor.executor_from_config.assert_called_once()
+    assert mock_disabled_kubernetes_cluster.runner is not None
+    assert mock_disabled_kubernetes_cluster.deferred is not None
+    mock_disabled_kubernetes_cluster.deferred.addCallback.assert_has_calls(
+        [
+            mock.call(mock_disabled_kubernetes_cluster.process_event),
+            mock.call(mock_disabled_kubernetes_cluster.handle_next_event),
+        ]
+    )
+
+
+def test_set_enabled_disable(mock_kubernetes_cluster):
+    mock_task = mock.Mock(spec=KubernetesTask)
+    mock_kubernetes_cluster.tasks == {"a.b": mock_task}
+
+    mock_kubernetes_cluster.set_enabled(is_enabled=False)
+
+    assert mock_kubernetes_cluster.enabled is False
+    mock_kubernetes_cluster.runner.stop.assert_called_once()
+    assert mock_kubernetes_cluster.deferred is None
+    assert mock_kubernetes_cluster.tasks == {}
--- tron/kubernetes.py
+++ tron/kubernetes.py
@@ -1,14 +1,28 @@
 import logging
 from logging import Logger
+from typing import Dict
+from typing import List
+from typing import Optional
 
-from task_processing.interfaces.event import Event  # type: ignore  # TODO: mypy isn't finding hints
-from task_processing.plugins.kubernetes.task_config import KubernetesTaskConfig  # type: ignore  # TODO: mypy isn't finding hints
+from task_processing.interfaces.event import Event  # type: ignore  # need to add task_proc type hints
+from task_processing.plugins.kubernetes.task_config import KubernetesTaskConfig  # type: ignore
+from task_processing.runners.subscription import Subscription  # type: ignore
+from task_processing.task_processor import TaskProcessor  # type: ignore
+from twisted.internet.defer import Deferred  # type: ignore  # need to upgrade twisted
+from twisted.internet.defer import logError
 
 import tron.metrics as metrics
 from tron.actioncommand import ActionCommand
+from tron.config.schema import ConfigVolume
+from tron.utils.queue import PyDeferredQueue
+
+
+DEFAULT_POD_LAUNCH_TIMEOUT_S = 300  # arbitrary number, same as Mesos offer timeout of yore
 
 KUBERNETES_TASK_LOG_FORMAT = "%(asctime)s %(name)s %(levelname)s %(message)s"
 KUBERNETES_TASK_OUTPUT_LOGGER = "tron.kubernetes.task_output"
+
+log = logging.getLogger(__name__)
 
 
 class KubernetesTask(ActionCommand):
@@ -27,17 +41,17 @@ class KubernetesTask(ActionCommand):
         Used to make it easier to disambiguate what the log messages emitted
         for event handling and such belong to.
         """
-        log = logging.getLogger(f"{__name__}.{self.id}")
+        event_log = logging.getLogger(f"{__name__}.{self.id}")
         # Every time a task gets created, this function runs and will add
         # more stderr handlers to the logger, which results in duplicate log
         # output. We only want to add the stderr handler if the logger does not
         # have a handler yet.
-        if not len(log.handlers):
+        if not len(event_log.handlers):
             handler = logging.StreamHandler(self.stderr)
             handler.setFormatter(logging.Formatter(KUBERNETES_TASK_LOG_FORMAT))
-            log.addHandler(handler)
+            event_log.addHandler(handler)
 
-        return log
+        return event_log
 
     def report_resources(self, decrement: bool = False) -> None:
         """
@@ -91,3 +105,206 @@ class KubernetesTask(ActionCommand):
             self.log.exception(f"Unable to log event info for id={event_id}.")
 
         # TODO(TRON-1611): actually transition to different states
+
+
+class KubernetesCluster:
+    def __init__(
+        self,
+        kubeconfig_path: str,
+        enabled: bool = True,
+        default_volumes: Optional[List[ConfigVolume]] = None,
+        pod_launch_timeout: Optional[int] = None,
+    ):
+        # general k8s config
+        self.kubeconfig_path = kubeconfig_path
+        self.enabled = enabled
+        self.default_volumes = default_volumes or []
+        self.pod_launch_timeout = pod_launch_timeout or DEFAULT_POD_LAUNCH_TIMEOUT_S
+
+        # creating a task_proc executor has a couple steps:
+        # * create a TaskProcessor
+        # * load the desired plugin (in this case, the k8s one)
+        # * and then actually create the executor (which we call a runner in tron)
+        # this last step requires a bit of setup, which is why we don't do it in-line
+        # in this constructor
+        self.processor = TaskProcessor()
+        self.processor.load_plugin(provider_module="task_processing.plugins.kubernetes")
+        self.runner: Optional[Subscription] = None
+
+        # queue to to use for tron<->task_proc communication - will hold k8s events seen
+        # by task_processing and held for tron to process.
+        self.queue = PyDeferredQueue()
+        # this will hold the current event to process (retrieved from the PyDeferredQueue above)
+        # which we will eventually wrap with some callbacks to actually process using the Twisted
+        # reactor started as part of tron's startup process
+        self.deferred: Optional[Deferred] = None
+
+        # map from k8s pod names to the task that said pod corresponds to
+        self.tasks: Dict[str, KubernetesTask] = {}
+
+        # actually create the executor/runner, as mentioned above.
+        self.connect()
+
+    def connect(self) -> None:
+        """
+        Starts running our Kubernetes task_processing.
+        """
+        self.runner = self.get_runner(kubeconfig_path=self.kubeconfig_path, queue=self.queue)
+        self.handle_next_event()
+
+    def get_runner(self, kubeconfig_path: str, queue: PyDeferredQueue) -> Optional[Subscription]:
+        """
+        Gets or creates an instance of our Kubernetes task_processing plugin.
+        """
+        if not self.enabled:
+            log.info("Kubernetes usage is disabled, not creating a runner.")
+            return None
+
+        # TODO: Add a stopping/terminating state to the task_proc runner
+        if self.runner is not None:
+            log.info("Reusing previously created runner.")
+            return self.runner
+
+        # TODO: once we start implementing more things in the executor, we'll need to actually pass
+        # down some config
+        executor = self.processor.executor_from_config(provider="kubernetes", provider_config={})
+
+        # TODO: we should return a Subscription here: but this will crash until we've actually implemented
+        # get_event_queue() in our task_proc plugin
+        return executor
+
+    def handle_next_event(self, _=None) -> None:
+        """
+        Pops events off of the shared tron<->task_proc queue and processes them.
+
+        We only expect two types of events:
+        * control: events regarding how the task_proc plugin is running - handled directly
+        * task: events regarding how the actual tasks/Pods we're running our doing - forwarded to KubernetesTask
+        """
+        if self.deferred and not self.deferred.called:
+            log.warning("Already have handlers waiting for next event in queue, not adding more")
+            return
+
+        self.deferred = self.queue.get()
+
+        # we want to process the event we just popped off the queue, but we also want
+        # to form a sort of event loop, so we add two callbacks:
+        # * one to actually deal with the event
+        # * and another to grab the next event, in this way creating an event loop :)
+        self.deferred.addCallback(self.process_event)
+        self.deferred.addCallback(self.handle_next_event)
+
+        # should an exception be thrown, these callbacks will be run instead
+        self.deferred.addErrback(logError)
+        self.deferred.addErrback(self.handle_next_event)
+
+    def process_event(self, event: Event) -> None:
+        """
+        Central router for all events received from task_processing.
+        """
+        if event.kind == "control":
+            self._handle_control_event(event)
+        elif event.kind == "task":
+            self._handle_task_event(event)
+        else:
+            log.warning(f"Unknown event type ({event.kind}): {event}")
+
+    def _handle_control_event(self, event: Event) -> None:
+        """
+        Helper method to handle any control-plane events sent from task_processing.
+        """
+        message = getattr(event, "message", None)
+        log.info(f"Processing a control event with message: {message}")
+
+    def _handle_task_event(self, event: Event) -> None:
+        """
+        Helper method to correctly route task-related events to the appropiate task.
+        """
+        task_id = getattr(event, "task_id", None)
+        if task_id is None:
+            log.warning(f"Received a malformed event with no task_id: {event}")
+            return
+
+        if task_id not in self.tasks.keys():
+            log.warning(f"Got event for unknown task ({task_id} not in {self.tasks.keys()}): {event}")
+            return
+
+        task = self.tasks[task_id]
+        task.handle_event(event)
+        if task.is_done:
+            del self.tasks[event.task_id]
+
+    def kill(self, task_id: str) -> bool:
+        """
+        Instructs task_processing to stop running a given task given a Pod name.
+        """
+        return self.runner.kill(task_id)  # type: ignore  # we need to add type annotation to task_proc
+
+    def stop(self, fail_tasks: bool = False) -> None:
+        """
+        Stops the configured task_processing runner and optionally fails all currently running tasks.
+
+        Will also clear the message queue and any unprocessed events.
+        """
+        if self.runner:
+            self.runner.stop()
+
+        # Clear message queue
+        if self.deferred:
+            self.deferred.cancel()
+            self.deferred = None
+        self.queue = PyDeferredQueue()
+
+        if fail_tasks:
+            for key, task in self.tasks.items():
+                # set the task status to unknown
+                task.exited(exit_status=None)
+                del self.tasks[key]
+
+    def set_enabled(self, is_enabled: bool) -> None:
+        """
+        Toggles use of the configured Kubernetes cluster.
+
+        Will fail all running tasks if toggled off.
+        """
+        self.enabled = is_enabled
+        if self.enabled:
+            self.connect()
+        else:
+            self.stop(fail_tasks=True)
+
+    def create_task(self, action_run_id: str, serializer, task_id: Optional[str] = None) -> Optional[KubernetesTask]:
+        """
+        Given the execution parameters for a task, create a KubernetesTask that encapsulate those parameters.
+
+        This task will not actually be run until KubernetesCluster::submit() is called.
+        """
+        if self.runner is None:
+            log.error(
+                f"Attempted to create a task for {action_run_id}, but no task_processing runner has been started."
+            )
+            return None
+
+        # TODO: fill out required fields once they're ready
+        task_config = self.runner.TASK_CONFIG_INTERFACE()
+
+        if task_id is not None:
+            try:
+                task_config = task_config.set_pod_name(task_id)
+            except ValueError:
+                log.error(f"Invalid {task_id} for {action_run_id}")
+                return None
+
+        return KubernetesTask(action_run_id=action_run_id, task_config=task_config, serializer=serializer)
+
+    def submit(self, task: KubernetesTask) -> None:
+        """
+        Given a KubernetesTask, submit it to the configured Kubernetes cluster in order to attempt to run it.
+        """
+        pass
+
+    def recover(self, task: KubernetesTask) -> None:
+        """
+        Given an instance of a KubernetesTask, attempt to reconcile the current state of the task from Kubernetes.
+        """
+        pass
