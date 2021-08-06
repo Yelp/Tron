282c174d2d7b640f95a8c02c26b8e35b2c32b303
Charles Wan <kawaiwan@yelp.com>
2021-08-05 17:36:44 -0700

    Make tronctl backfill watch job runs until they complete serially

--- tests/commands/backfill_test.py
+++ tests/commands/backfill_test.py
@@ -11,6 +11,12 @@ TEST_DATETIME_2 = datetime.datetime.strptime("2004-07-02", "%Y-%m-%d")
 
 
 @pytest.fixture(autouse=True)
+def mock_sleep():
+    with mock.patch("time.sleep", autospec=None, return_value=None):
+        yield
+
+
+@pytest.fixture(autouse=True)
 def mock_client():
     with mock.patch.object(client, "Client", autospec=True) as m:
         yield m
@@ -51,40 +57,79 @@ def test_run_backfill_for_date_range_create_only(
     mock_get_obj_type.return_value = client.TronObjectIdentifier("JOB", "a_url")
     mock_create_job_run.side_effect = [None, ""]
 
-    results = list(backfill.run_backfill_for_date_range("a_server", "a_job", [TEST_DATETIME_1, TEST_DATETIME_2]))
+    results = list(
+        backfill.run_backfill_for_date_range(
+            "a_server", "a_job", [TEST_DATETIME_1, TEST_DATETIME_2], ignore_errors=False,
+        ),
+    )
 
     assert results == [False, True]
     assert mock_create_job_run.call_args_list == [
-        mock.call("a_server", "a_job", "a_url", TEST_DATETIME_1),
-        mock.call("a_server", "a_job", "a_url", TEST_DATETIME_2),
+        mock.call("a_server", "a_url", TEST_DATETIME_1),
+        mock.call("a_server", "a_url", TEST_DATETIME_2),
     ]
 
 
+@pytest.mark.parametrize("ignore_errors,expected", [(False, [False, True]), (True, [True, True])])
 @mock.patch.object(client, "get_object_type_from_identifier", autospec=True)
 @mock.patch.object(backfill, "_create_job_run", autospec=True)
-def test_run_backfill_for_date_range_no_job_run_name(
-    mock_create_job_run, mock_get_obj_type,
+@mock.patch.object(backfill, "_watch_job_run", autospec=True)
+def test_run_backfill_for_date_range_watch(
+    mock_watch_job_run, mock_create_job_run, mock_get_obj_type, mock_client, ignore_errors, expected,
 ):
     mock_get_obj_type.return_value = client.TronObjectIdentifier("JOB", "a_url")
-    mock_create_job_run.return_value = ""
+    mock_create_job_run.return_value = "a_job_run"
+    mock_watch_job_run.side_effect = [False, True]
 
-    results = list(backfill.run_backfill_for_date_range("a_server", "a_job", [TEST_DATETIME_1]))
+    results = list(
+        backfill.run_backfill_for_date_range(
+            "a_server", "a_job", [TEST_DATETIME_1, TEST_DATETIME_2], ignore_errors=ignore_errors,
+        ),
+    )
 
-    assert results == [True]
+    assert results == expected
     assert mock_create_job_run.call_args_list == [
-        mock.call("a_server", "a_job", "a_url", TEST_DATETIME_1),
+        mock.call("a_server", "a_url", TEST_DATETIME_1),
+        mock.call("a_server", "a_url", TEST_DATETIME_2),
+    ]
+    assert mock_watch_job_run.call_args_list == [
+        mock.call(mock_client.return_value, "a_job_run", "2004-07-01"),
+        mock.call(mock_client.return_value, "a_job_run", "2004-07-02"),
     ]
 
 
 @pytest.mark.parametrize(
     "is_error,result,expected",
     [
-        (True, "an_error_msg", None),
-        (False, "weird_resp_msg", ""),
-        (False, "Created JobRun:real_job_run_name", "real_job_run_name"),
+        (True, "an_error_msg", None),  # tron api failed
+        (False, "weird_resp_msg", ""),  # bad response, can't get job run name
+        (False, "Created JobRun:real_job_run_name", "real_job_run_name"),  # ok
     ],
 )
 def test_create_job_run(mock_client_request, is_error, result, expected):
     mock_client_request.return_value.error = is_error
     mock_client_request.return_value.content["result"] = result
-    assert backfill._create_job_run("a_server", "a_job", "http://localhost", TEST_DATETIME_1) == expected
+
+    assert expected == backfill._create_job_run("a_server", "http://localhost", TEST_DATETIME_1)
+
+
+@pytest.mark.parametrize(
+    "job_run_resp,poll_cnt,expected",
+    [
+        (client.RequestError, 1, False),  # polling failed
+        ([{}], 1, False),  # default to unknown
+        ([{"state": "running"}, {"state": "failed"}], 2, False),  # job run fail
+        ([{"state": "running"}, {"state": "succeeded"}], 2, True),  # ok
+    ],
+)
+@mock.patch.object(client, "get_object_type_from_identifier", autospec=True)
+def test_watch_job_run(mock_get_obj_type, mock_client, job_run_resp, poll_cnt, expected):
+    mock_get_obj_type.return_value = client.TronObjectIdentifier("JOB_RUN", "a_url")
+    tron_client = mock_client.return_value
+    tron_client.url_base = "http://localhost"
+    tron_client.job_runs.side_effect = job_run_resp
+
+    run_successful = backfill._watch_job_run(tron_client, "a_job_run", "2004-07-01")
+
+    assert expected == run_successful
+    assert poll_cnt == len(tron_client.job_runs.call_args_list)
--- tron/commands/backfill.py
+++ tron/commands/backfill.py
@@ -1,13 +1,16 @@
 import datetime
 import pprint
 import re
+import time
 from typing import Generator
 from typing import List
 from urllib.parse import urljoin
 
 from tron.commands import client
+from tron.core.actionrun import ActionRun
 
 DEFAULT_MAX_PARALLEL_RUNS = 10
+DEFAULT_POLLING_INTERVAL = 10  # seconds
 
 
 def get_date_range(
@@ -49,7 +52,11 @@ def confirm_backfill(job: str, date_strs: List[str]):
 
 
 def run_backfill_for_date_range(
-    server: str, job_name: str, dates: List[datetime.datetime], max_parallel: int = DEFAULT_MAX_PARALLEL_RUNS,
+    server: str,
+    job_name: str,
+    dates: List[datetime.datetime],
+    max_parallel: int = DEFAULT_MAX_PARALLEL_RUNS,
+    ignore_errors: bool = True,
 ) -> Generator[bool, None, None]:
     """Creates and watches job runs over a range of dates for a given job. At
     most, max_parallel runs can run in parallel to prevent resource exhaustion.
@@ -65,30 +72,37 @@ def run_backfill_for_date_range(
 
     for run_time in dates:
         date_str = run_time.date().isoformat()
-        job_run_name = _create_job_run(server, job_name, job_id.url, run_time)
+        job_run_name = _create_job_run(server, job_id.url, run_time)
+        run_successful = True
+
         if job_run_name is None:
-            yield False
+            run_successful = False
         elif job_run_name == "":
             print(
                 f"Warning: Job run for {date_str} created, but couldn't determine "
                 "its name, so it will not be watched for completion"
             )
-            yield True
         else:
-            print(f"Created for job run '{job_run_name}' for {date_str}")
-            yield True
+            print(f"Waiting for job run '{job_run_name}' for {date_str} to finish...")
+            run_successful = _watch_job_run(tron_client, job_run_name, date_str)
+
+        yield run_successful or ignore_errors
 
 
-def _create_job_run(server: str, job_name: str, job_url: str, run_time: datetime.datetime,) -> bool:
-    """Creates and watches a job run for a specific date.
+def _create_job_run(server: str, job_url: str, run_time: datetime.datetime,) -> bool:
+    """Creates job run for a specific date.
 
-    Returns True if the job run completed successfully, and False otherwise.
+    Returns:
+        None, if the job run couldn't be created
+        "", if the job run was created, but its name couldn't be determined
+        the job run's name, otherwise
     """
     # create the job run
     data = dict(command="start", run_time=run_time)
     response = client.request(urljoin(server, job_url), data=data)
     if response.error:
-        print(f"Error: {response.content}")
+        date_str = run_time.date().isoformat()
+        print(f"Error: couldn't start job run for {date_str}: {response.content}")
         return None
 
     # determine name of job run so that we can watch it
@@ -97,3 +111,32 @@ def _create_job_run(server: str, job_name: str, job_url: str, run_time: datetime
     result = response.content.get("result")
     match = re.match(r"^Created JobRun:([-.\w]+)$", result)
     return match.groups(0)[0] if match else ""
+
+
+def _watch_job_run(
+    tron_client: client.Client, job_run_name: str, date_str: str, poll_intv_s: int = DEFAULT_POLLING_INTERVAL,
+) -> bool:
+    """Watches a job run until it completes.
+
+    Returns:
+        True, if the job run completed successfully
+        False, otherwise
+    """
+    job_run_id = client.get_object_type_from_identifier(tron_client.index(), job_run_name)
+    job_run_state = None
+
+    # job run states are derived form action run states
+    # see: tron.core.jobrun.JobRun.state
+    while job_run_state not in ActionRun.END_STATES:
+        time.sleep(poll_intv_s)
+        try:
+            resp_content = tron_client.job_runs(
+                urljoin(tron_client.url_base, job_run_id.url), include_runs=False, include_graph=False,
+            )
+        except client.RequestError as e:
+            print(f"Error: couldn't get state for job run '{job_run_name}': {e}")
+            return False
+        job_run_state = resp_content.get("state", ActionRun.UNKNOWN)
+
+    print(f"Job run '{job_run_name}' for {date_str} finished with state: {job_run_state}")
+    return job_run_state == ActionRun.SUCCEEDED
