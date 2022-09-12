5d612cd864d83e03eaecadddbe9fd713ca8b0849
Jon Lee <jonlee@yelp.com>
2022-09-12 10:59:05 -0700

    fixes broken test

--- tests/rookout_test.py
+++ tests/rookout_test.py
@@ -14,9 +14,15 @@ def test_enable_rookout_not_configured(rook_start: MagicMock):
 
 @patch("rook.start")
 @patch("tron.utils.rookout.prepare_rookout_token")
+@patch("tron.utils.habitat.get_superregion")
+@patch("tron.utils.habitat.get_ecosystem")
 @patch("tron.utils.rookout.ROOKOUT_ENABLE", True)
-def test_enable_rookout(prepare_rookout_token: MagicMock, rook_start: MagicMock):
+def test_enable_rookout(
+    get_ecosystem: MagicMock, get_superregion: MagicMock, prepare_rookout_token: MagicMock, rook_start: MagicMock
+):
     rook_start.return_value = True
+    get_ecosystem.return_value = "test_ecosystem"
+    get_superregion.return_value = "test_superregion"
     prepare_rookout_token.return_value = "not-a-real-token"
     enable_rookout()
     assert rook_start.call_count == 1
