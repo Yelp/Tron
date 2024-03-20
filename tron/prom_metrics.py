d49d236300f9e6fde3390329e978dba176d7a226
Eman Elsabban <emanelsabban@yelp.com>
2024-03-20 07:21:10 -0700

    Editing some of the messages from previous prs

--- tron/prom_metrics.py
+++ tron/prom_metrics.py
@@ -1,6 +1,6 @@
 from prometheus_client import Gauge
 
 
-tron_cpu_gauge = Gauge("tron_k8s_cpus", "Measuring CPU for tron jobs on K8s")
-tron_memory_gauge = Gauge("tron_k8s_mem", "Measuring memory for tron jobs on K8s")
-tron_disk_gauge = Gauge("tron_k8s_disk", "Measuring disk for tron jobs on K8s")
+tron_cpu_gauge = Gauge("tron_k8s_cpus", "Total number of CPUs allocated to Tron-launched containers")
+tron_memory_gauge = Gauge("tron_k8s_mem", "Total amount of memory allocated to Tron-launched containers (in megabytes)")
+tron_disk_gauge = Gauge("tron_k8s_disk", "Total amount of disk allocated to Tron-launched containers (in megabytes)")
