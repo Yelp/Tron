95df55c8191597e0b99ce0e40069ae147fa44c55
Yaroslav Liakhovskyi <yaro@yelp.com>
2024-12-16 06:16:09 -0800

    Remove scribe readers from Tron logs module

--- tests/utils/scribereader_test.py
+++ tests/utils/scribereader_test.py
@@ -21,9 +21,7 @@ def static_conf_patch(args):
 
 
 def test_read_log_stream_for_action_run_not_available():
-    with mock.patch("tron.utils.scribereader.scribereader_available", False), mock.patch(
-        "tron.utils.scribereader.s3reader_available", False
-    ):
+    with mock.patch("tron.utils.scribereader.s3reader_available", False):
         output = tron.utils.scribereader.read_log_stream_for_action_run(
             "namespace.job.1234.action",
             component="stdout",
@@ -34,16 +32,14 @@ def test_read_log_stream_for_action_run_not_available():
     assert "unable to display logs" in output[0]
 
 
-def test_read_log_stream_for_action_run_yelp_clog():
+def test_read_log_stream_for_action_run():
     with mock.patch(
         "staticconf.read",
         autospec=True,
-        side_effect=static_conf_patch({"logging.use_s3_reader": True, "logging.max_lines_to_display": 1000}),
+        side_effect=static_conf_patch({"logging.max_lines_to_display": 1000}),
     ), mock.patch("tron.config.static_config.build_configuration_watcher", autospec=True,), mock.patch(
         "tron.config.static_config.load_yaml_file",
         autospec=True,
-    ), mock.patch(
-        "tron.utils.scribereader.get_ecosystem", autospec=True, return_value="fake"
     ), mock.patch(
         "tron.utils.scribereader.get_superregion", autospec=True, return_value="fake"
     ), mock.patch(
@@ -83,6 +79,10 @@ def test_read_log_stream_for_action_run_yelp_clog():
             max_date=datetime.datetime.now(),
             paasta_cluster="fake",
         )
+
+    mock_s3_reader.return_value.get_log_reader.assert_called_once_with(
+        log_name="stream_paasta_app_output_namespace_job__action", start_datetime=mock.ANY, end_datetime=mock.ANY
+    )
     assert output == ["line 1", "line 2"]
 
 
@@ -99,16 +99,14 @@ def test_read_log_stream_for_action_run_yelp_clog():
         ),
     ],
 )
-def test_read_log_stream_for_action_run_yelp_clog_tz(local_datetime, expected_datetime):
+def test_read_log_stream_for_action_run_tz(local_datetime, expected_datetime):
     with mock.patch(
         "staticconf.read",
         autospec=True,
-        side_effect=static_conf_patch({"logging.use_s3_reader": True, "logging.max_lines_to_display": 1000}),
+        side_effect=static_conf_patch({"logging.max_lines_to_display": 1000}),
     ), mock.patch("tron.config.static_config.build_configuration_watcher", autospec=True,), mock.patch(
         "tron.config.static_config.load_yaml_file",
         autospec=True,
-    ), mock.patch(
-        "tron.utils.scribereader.get_ecosystem", autospec=True, return_value="fake"
     ), mock.patch(
         "tron.utils.scribereader.get_superregion", autospec=True, return_value="fake"
     ), mock.patch(
@@ -127,267 +125,12 @@ def test_read_log_stream_for_action_run_yelp_clog_tz(local_datetime, expected_da
     )
 
 
-def test_read_log_stream_for_action_run_min_date_and_max_date_today():
-    # NOTE: these tests don't actually depend on the current time apart from
-    # today vs not-today and the args are forwarded to scribereader anyway
-    # so using the current time is fine
-    min_date = datetime.datetime.now()
-    max_date = datetime.datetime.now() + datetime.timedelta(hours=1)
-    with mock.patch(
-        "tron.utils.scribereader.get_scribereader_host_and_port",
-        autospec=True,
-        return_value=("host", 1234),
-    ), mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_reader",
-        autospec=True,
-    ) as mock_stream_reader, mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_tailer",
-        autospec=True,
-    ) as mock_stream_tailer, mock.patch(
-        "tron.utils.scribereader.get_superregion",
-        autospec=True,
-        return_value="fake",
-    ), mock.patch(
-        "tron.config.static_config.build_configuration_watcher",
-        autospec=True,
-    ), mock.patch(
-        "staticconf.read", autospec=True, side_effect=static_conf_patch({"logging.max_lines_to_display": 1000})
-    ), mock.patch(
-        "tron.config.static_config.load_yaml_file",
-        autospec=True,
-    ):
-        # in this case, we shouldn't even try to check the reader, so lets set an exception
-        # to make sure we didn't try
-        mock_stream_reader.return_value.__enter__.side_effect = Exception
-        # but the tailer should have some data and actually be used
-        mock_stream_tailer.return_value.__iter__.return_value = iter(
-            [
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 1",
-                "timestamp": "2021-01-02T18:10:09.169421619Z",
-                "cluster": "fake"
-            }""",
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 2",
-                "timestamp": "2021-01-02T18:11:09.169421619Z",
-                "cluster": "fake"
-            }""",
-                """{
-                "tron_run_number": 1234,
-                "component": "stderr",
-                "message": "line 3",
-                "timestamp": "2021-01-02T18:12:09.169421619Z",
-                "cluster": "fake"
-            }""",
-            ]
-        )
-        output = read_log_stream_for_action_run(
-            action_run_id="namespace.job.1234.action",
-            component="stdout",
-            min_date=min_date,
-            max_date=max_date,
-            paasta_cluster="fake",
-        )
-
-    mock_stream_reader.assert_not_called()
-    mock_stream_tailer.assert_called_once_with(
-        stream_name="stream_paasta_app_output_namespace_job__action",
-        lines=-1,
-        tailing_host="host",
-        tailing_port=1234,
-    )
-    assert output == ["line 1", "line 2"]
-
-
-def test_read_log_stream_for_action_run_min_date_and_max_date_different_days():
-    # NOTE: these tests don't actually depend on the current time apart from
-    # today vs not-today and the args are forwarded to scribereader anyway
-    # so using the current time is fine
-    min_date = datetime.datetime.now() - datetime.timedelta(days=5)
-    max_date = datetime.datetime.now()
-    with mock.patch(
-        "tron.utils.scribereader.get_scribereader_host_and_port",
-        autospec=True,
-        return_value=("host", 1234),
-    ), mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_reader",
-        autospec=True,
-    ) as mock_stream_reader, mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_tailer",
-        autospec=True,
-    ) as mock_stream_tailer, mock.patch(
-        "tron.utils.scribereader.get_superregion",
-        autospec=True,
-        return_value="fake",
-    ), mock.patch(
-        "tron.config.static_config.build_configuration_watcher",
-        autospec=True,
-    ), mock.patch(
-        "staticconf.read", autospec=True, side_effect=static_conf_patch({"logging.max_lines_to_display": 1000})
-    ), mock.patch(
-        "tron.config.static_config.load_yaml_file",
-        autospec=True,
-    ):
-        # we should check the reader for data from a previous day
-        mock_stream_reader.return_value.__enter__.return_value = iter(
-            [
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 0",
-                "timestamp": "2021-01-02T18:10:09.169421619Z",
-                "cluster": "fake"
-            }""",
-            ]
-        )
-        # but then check the tailer for todays data
-        mock_stream_tailer.return_value.__iter__.return_value = iter(
-            [
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 1",
-                "timestamp": "2021-01-02T18:10:09.169421619Z",
-                "cluster": "fake"
-            }""",
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 2",
-                "timestamp": "2021-01-02T18:11:09.169421619Z",
-                "cluster": "fake"
-            }""",
-                """{
-                "tron_run_number": 1234,
-                "component": "stderr",
-                "message": "line 3",
-                "timestamp": "2021-01-02T18:12:09.169421619Z",
-                "cluster": "fake"
-            }""",
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 4",
-                "timestamp": "2021-01-02T18:12:09.169421619Z",
-                "cluster": "bad_fake"
-            }""",
-            ]
-        )
-        output = read_log_stream_for_action_run(
-            action_run_id="namespace.job.1234.action",
-            component="stdout",
-            min_date=min_date,
-            max_date=max_date,
-            paasta_cluster="fake",
-        )
-
-    mock_stream_reader.assert_called_once_with(
-        stream_name="stream_paasta_app_output_namespace_job__action",
-        min_date=min_date,
-        max_date=max_date,
-        reader_host="host",
-        reader_port=1234,
-    )
-    mock_stream_tailer.assert_called_once_with(
-        stream_name="stream_paasta_app_output_namespace_job__action",
-        lines=-1,
-        tailing_host="host",
-        tailing_port=1234,
-    )
-    assert output == ["line 0", "line 1", "line 2"]
-
-
-def test_read_log_stream_for_action_run_min_date_and_max_date_in_past():
-    # NOTE: these tests don't actually depend on the current time apart from
-    # today vs not-today and the args are forwarded to scribereader anyway
-    # so using the current time is fine
-    min_date = datetime.datetime.now() - datetime.timedelta(days=5)
-    max_date = datetime.datetime.now() - datetime.timedelta(days=4)
-    with mock.patch(
-        "tron.utils.scribereader.get_scribereader_host_and_port",
-        autospec=True,
-        return_value=("host", 1234),
-    ), mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_reader",
-        autospec=True,
-    ) as mock_stream_reader, mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_tailer",
-        autospec=True,
-    ) as mock_stream_tailer, mock.patch(
-        "tron.utils.scribereader.get_superregion",
-        autospec=True,
-        return_value="fake",
-    ), mock.patch(
-        "tron.config.static_config.build_configuration_watcher",
-        autospec=True,
-    ), mock.patch(
-        "staticconf.read", autospec=True, side_effect=static_conf_patch({"logging.max_lines_to_display": 1000})
-    ), mock.patch(
-        "tron.config.static_config.load_yaml_file",
-        autospec=True,
-    ):
-        # all the data we want is from the past, so we should only check the reader
-        mock_stream_reader.return_value.__enter__.return_value = iter(
-            [
-                """{
-                "tron_run_number": 1234,
-                "component": "stdout",
-                "message": "line 0",
-                "timestamp": "2021-01-02T18:10:09.169421619Z",
-                "cluster": "fake"
-            }""",
-            ]
-        )
-        # so lets make sure we don't call the tailer
-        mock_stream_tailer.return_value.__iter__.side_effect = Exception
-        output = read_log_stream_for_action_run(
-            action_run_id="namespace.job.1234.action",
-            component="stdout",
-            min_date=min_date,
-            max_date=max_date,
-            paasta_cluster="fake",
-        )
-
-    mock_stream_reader.assert_called_once_with(
-        stream_name="stream_paasta_app_output_namespace_job__action",
-        min_date=min_date,
-        max_date=max_date,
-        reader_host="host",
-        reader_port=1234,
-    )
-    mock_stream_tailer.assert_not_called()
-    assert output == ["line 0"]
-
-
-def test_read_log_stream_for_action_run_min_date_and_max_date_for_long_output():
-    # NOTE: these tests don't actually depend on the current time apart from
-    # today vs not-today and the args are forwarded to scribereader anyway
-    # so using the current time is fine
-    min_date = datetime.datetime.now() - datetime.timedelta(days=5)
-    max_date = datetime.datetime.now() - datetime.timedelta(days=4)
+def test_read_log_stream_for_action_run_for_long_output():
     # 1000 represents the number of lines that are expected to be
     # outputted by the test, which is similar to the logging.max_lines_to_display
     # in tron.yaml in srv-configs
     max_lines = 1000
-    with mock.patch(
-        "tron.utils.scribereader.get_scribereader_host_and_port",
-        autospec=True,
-        return_value=("host", 1234),
-    ), mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_reader",
-        autospec=True,
-    ) as mock_stream_reader, mock.patch(
-        "tron.utils.scribereader.scribereader.get_stream_tailer",
-        autospec=True,
-    ) as mock_stream_tailer, mock.patch(
-        "tron.utils.scribereader.get_superregion",
-        autospec=True,
-        return_value="fake",
-    ), mock.patch(
+    with mock.patch("tron.utils.scribereader.get_superregion", autospec=True, return_value="fake",), mock.patch(
         "tron.config.static_config.build_configuration_watcher",
         autospec=True,
     ), mock.patch(
@@ -395,32 +138,26 @@ def test_read_log_stream_for_action_run_min_date_and_max_date_for_long_output():
     ), mock.patch(
         "tron.config.static_config.load_yaml_file",
         autospec=True,
-    ):
+    ), mock.patch(
+        "tron.utils.scribereader.S3LogsReader", autospec=True
+    ) as mock_s3_reader:
 
         with open("./tests/utils/shortOutputTest.txt") as f:
             content_list = f.readlines()
 
-        mock_stream_reader.return_value.__enter__.return_value = iter(content_list)
+        mock_s3_reader.return_value.get_log_reader.return_value = iter(content_list)
 
-        # so lets make sure we don't call the tailer
-        mock_stream_tailer.return_value.__iter__.side_effect = Exception
         output = read_log_stream_for_action_run(
-            action_run_id="namespace.job.228.action",
+            "namespace.job.228.action",
             component="stdout",
-            min_date=min_date,
-            max_date=max_date,
+            min_date=datetime.datetime.now(),
+            max_date=datetime.datetime.now(),
             paasta_cluster="infrastage",
         )
-    mock_stream_reader.assert_called_once_with(
-        stream_name="stream_paasta_app_output_namespace_job__action",
-        min_date=min_date,
-        max_date=max_date,
-        reader_host="host",
-        reader_port=1234,
+
+    mock_s3_reader.return_value.get_log_reader.assert_called_once_with(
+        log_name="stream_paasta_app_output_namespace_job__action", start_datetime=mock.ANY, end_datetime=mock.ANY
     )
-    mock_stream_tailer.assert_not_called()
-    # The expected output should be max_lines plus the
-    # extra line for 'This output is truncated.' message
     assert len(output) == max_lines + 1
 
 
--- tron/utils/scribereader.py
+++ tron/utils/scribereader.py
@@ -2,7 +2,6 @@ import datetime
 import json
 import logging
 import operator
-import socket
 from functools import lru_cache
 from typing import Iterator
 from typing import List
@@ -14,15 +13,6 @@ import yaml
 
 from tron.config.static_config import get_config_watcher
 from tron.config.static_config import NAMESPACE
-
-
-try:
-    from scribereader import scribereader
-    from scribereader.clog.readers import StreamTailerSetupError
-
-    scribereader_available = True
-except ImportError:
-    scribereader_available = False  # sorry folks, you'll need to add your own way to retrieve logs
 
 try:
     from clog.readers import S3LogsReader
@@ -37,17 +27,6 @@ USE_SRV_CONFIGS = -1
 
 
 @lru_cache(maxsize=1)
-def get_region() -> str:
-    """
-    Discover what region we're running in by reading this information from on-disk facts.
-
-    Yelpers: for more information, see y/habitat
-    """
-    with open("/nail/etc/region") as f:
-        return f.read().strip()
-
-
-@lru_cache(maxsize=1)
 def get_superregion() -> str:
     """
     Discover what region we're running in by reading this information from on-disk facts.
@@ -56,30 +35,6 @@ def get_superregion() -> str:
     """
     with open("/nail/etc/superregion") as f:
         return f.read().strip()
-
-
-@lru_cache(maxsize=1)
-def get_ecosystem() -> str:
-    """
-    Discover what ecosystem we're running in by reading this information from on-disk facts.
-
-    Yelpers: for more information, see y/habitat
-    """
-    with open("/nail/etc/ecosystem") as f:
-        return f.read().strip()
-
-
-@lru_cache(maxsize=1)
-def get_scribereader_host_and_port(ecosystem: str, superregion: str, region: str) -> Tuple[str, int]:
-    # NOTE: Passing in an ecosystem of prod is not supported by scribereader
-    # as there's no mapping of ecosystem->scribe-kafka-services discovery hosts
-    # for this ecosystem
-    host, port = scribereader.get_tail_host_and_port(
-        ecosystem=ecosystem if ecosystem != "prod" else None,
-        region=region,
-        superregion=superregion,
-    )
-    return host, port
 
 
 def decompose_action_id(action_run_id: str, paasta_cluster: str) -> Tuple[str, str, str, str]:
@@ -168,14 +123,8 @@ def read_log_stream_for_action_run(
     if min_date is None:
         return [f"{action_run_id} has not started yet."]
 
-    use_s3_reader = False
-
-    if s3reader_available:
-        config_watcher = get_config_watcher()
-        config_watcher.reload_if_changed()
-        use_s3_reader = staticconf.read("logging.use_s3_reader", namespace=NAMESPACE, default=False)
-    elif scribereader_available is False:
-        return ["Neither scribereader nor yelp_clog (internal Yelp packages) are available - unable to display logs."]
+    if not s3reader_available:
+        return ["yelp_clog (internal Yelp package) is not available - unable to display logs."]
 
     if max_lines == USE_SRV_CONFIGS:
         config_watcher = get_config_watcher()
@@ -183,11 +132,9 @@ def read_log_stream_for_action_run(
         max_lines = staticconf.read("logging.max_lines_to_display", namespace=NAMESPACE)
 
     try:
-        ecosystem = get_ecosystem()
         superregion = get_superregion()
-        region = get_region()
     except OSError:
-        log.warning("Unable to read location mapping files from disk, not returning scribereader host/port")
+        log.warning("Unable to read location mapping files from disk (/nail/etc/)")
         return [
             "Unable to determine where Tron is located. If you're seeing this inside Yelp, report this to #compute-infra"
         ]
@@ -199,92 +146,34 @@ def read_log_stream_for_action_run(
     stream_name = paasta_logs.stream_name
     end_date: Optional[datetime.date]
 
-    # yelp_clog S3LogsReader is a newer reader that is supposed to replace scribe readers eventually.
-    if use_s3_reader:
-        # S3 reader accepts datetime objects and respects timezone information
-        # if min_date and max_date timezone is missing, astimezone() will assume local timezone and convert it to UTC
-        start_datetime = min_date.astimezone(datetime.timezone.utc)
-        end_datetime = (
-            max_date.astimezone(datetime.timezone.utc)
-            if max_date
-            else datetime.datetime.now().astimezone(datetime.timezone.utc)
-        )
+    # S3 reader accepts datetime objects and respects timezone information
+    # if min_date and max_date timezone is missing, astimezone() will assume local timezone and convert it to UTC
+    start_datetime = min_date.astimezone(datetime.timezone.utc)
+    end_datetime = (
+        max_date.astimezone(datetime.timezone.utc)
+        if max_date
+        else datetime.datetime.now().astimezone(datetime.timezone.utc)
+    )
 
-        log.debug("Using S3LogsReader to retrieve logs")
-        s3_reader = S3LogsReader(superregion).get_log_reader(
-            log_name=stream_name, start_datetime=start_datetime, end_datetime=end_datetime
-        )
-        paasta_logs.fetch(s3_reader, max_lines)
-    else:
-        today = datetime.date.today()
-        start_date = min_date.date()
-        end_date = max_date.date() if max_date else None
-        use_tailer = today in {start_date, end_date}
-        use_reader = start_date != today and end_date is not None
+    log.debug("Using S3LogsReader to retrieve logs")
+    s3_reader = S3LogsReader(superregion).get_log_reader(
+        log_name=stream_name, start_datetime=start_datetime, end_datetime=end_datetime
+    )
+    paasta_logs.fetch(s3_reader, max_lines)
 
-        host, port = get_scribereader_host_and_port(ecosystem, superregion, region)
-
-        # We'll only use a stream reader for logs from not-today.
-        # that said, it's possible that an action spans more than a single day - in this case, we'll first read "historical" data from
-        # the reader and then follow-up with today's logs from a stream tailer.
-        # NOTE: this is more-or-less what our internal `scribereader` binary does
-        if use_reader:
-            with scribereader.get_stream_reader(
-                stream_name=stream_name,
-                min_date=min_date,
-                max_date=max_date,
-                reader_host=host,
-                reader_port=port,
-            ) as stream:
-                paasta_logs.fetch(stream, max_lines)
-
-        if use_tailer:
-            stream = scribereader.get_stream_tailer(
-                stream_name=stream_name,
-                tailing_host=host,
-                tailing_port=port,
-                lines=-1,
-            )
-            try:
-                paasta_logs.fetch(stream, max_lines)
-            except StreamTailerSetupError:
-                return [
-                    f"No data in stream {stream_name} - if this is the first time this action has run and you expected "
-                    "output, please wait a couple minutes and refresh."
-                ]
-            except socket.timeout:
-                return [
-                    f"Unable to connect to stream {stream_name} - if this is the first time this action has run and you "
-                    "expected output, please wait a couple minutes and refresh."
-                ]
-            finally:
-                stream.close()
-
-    # for logs that use Kafka topics with multiple partitions underneath or retrieved by S3LogsReader,
-    # data ordering is not guaranteed - so we'll sort based on log timestamp set by producer.
+    # S3LogsReader does not guarantee order of logs in the output - so we'll sort based on log timestamp set by producer.
     lines = paasta_logs.sorted_lines()
     malformed = (
         [f"{paasta_logs.malformed_lines} encountered while retrieving logs"] if paasta_logs.malformed_lines else []
     )
 
-    if use_s3_reader:
-        location_selector = f"-s {get_superregion()}"
-    else:
-        try:
-            location_selector = (
-                f"-s {paasta_cluster}" if "prod" in paasta_cluster else f'-e {paasta_cluster.split("-")[1]}'
-            )
-        except IndexError:
-            location_selector = f"-s {paasta_cluster}"
-
-    command_name = "logreader" if use_s3_reader else "scribereader"
     truncation_message = (
         [
-            f"This output is truncated. Use this command to view all lines: {command_name} {location_selector} {stream_name} --min-date {min_date.date()} --max-date {max_date.date()} | jq --raw-output 'select(.tron_run_number=={int(paasta_logs.run_num)} and .component == \"{component}\") | .message'"
+            f"This output is truncated. Use this command to view all lines: logreader -s {superregion} {stream_name} --min-date {min_date.date()} --max-date {max_date.date()} | jq --raw-output 'select(.tron_run_number=={int(paasta_logs.run_num)} and .component == \"{component}\") | .message'"
         ]
         if max_date
         else [
-            f"This output is truncated. Use this command to view all lines: {command_name} {location_selector} {stream_name} --min-date {min_date.date()} | jq --raw-output 'select(.tron_run_number=={int(paasta_logs.run_num)} and .component == \"{component}\") | .message'"
+            f"This output is truncated. Use this command to view all lines: logreader -s {superregion} {stream_name} --min-date {min_date.date()} | jq --raw-output 'select(.tron_run_number=={int(paasta_logs.run_num)} and .component == \"{component}\") | .message'"
         ]
     )
     truncated = truncation_message if paasta_logs.truncated_output else []
