e148ee5cd89ea5ae253bf1bcdf27fae5522ccf11
Kevin Kaspari <kkasp@yelp.com>
2025-05-12 12:44:55 -0700

    Add prometheus metric for partition histogram

--- tron/prom_metrics.py
+++ tron/prom_metrics.py
@@ -1,5 +1,6 @@
 from prometheus_client import Counter
 from prometheus_client import Gauge
+from prometheus_client import Histogram
 
 
 tron_cpu_gauge = Gauge("tron_k8s_cpus", "Total number of CPUs allocated to Tron-launched containers")
@@ -14,4 +15,12 @@ json_serialization_errors_counter = Counter(
 json_deserialization_errors_counter = Counter(
     "json_deserialization_errors_total",
     "Total number of errors encountered while deserializing state_data from JSON. These errors occur after reading from DynamoDB.",
+)
+
+# Our current peak is about 10-12 partitions, so this should be more than sufficient.
+# Anything above 20 would get grouped into an inf bucket until we expand this.
+tron_dynamodb_partitions_histogram = Histogram(
+    "tron_dynamodb_partitions",
+    "Distribution of partitions per item in DynamoDB",
+    buckets=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
 )
