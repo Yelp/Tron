30d50ab33c9213163b910e043a05f8e24b5c6ab7
Matteo Piano <mpiano@yelp.com>
2025-06-13 09:22:53 +0100

    cache auth tokens before starting backfill jobs (#1053)

--- tron/commands/backfill.py
+++ tron/commands/backfill.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import functools
+import os
 import pprint
 import re
 import signal
@@ -11,6 +12,7 @@ from urllib.parse import urljoin
 
 from tron.commands import client
 from tron.commands import display
+from tron.commands.authentication import get_auth_token
 from tron.core.actionrun import ActionRun
 
 DEFAULT_MAX_PARALLEL_RUNS = 3
@@ -228,6 +230,12 @@ async def run_backfill_for_date_range(
     most, max_parallel runs can run in parallel to prevent resource exhaustion.
     """
     loop = asyncio.get_event_loop()
+
+    # Trigger authentication before submitting all async jobs, so auth tokens
+    # are cached and won't prompt the user in the individual API calls
+    if os.getenv("TRONCTL_API_AUTH"):
+        get_auth_token()
+
     tron_client = client.Client(server, user_attribution=True)
     url_index = tron_client.index()
 
