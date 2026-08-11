74a45a0e935d80ab3b8ab2d6a6554cd863a1bcac
Charles Peacock <cmpa8884@gmail.com>
2026-08-11 11:59:07 -0600

    Change max_runtime from 168 hours to 7 days

--- tron/config/config_parse.py
+++ tron/config/config_parse.py
@@ -774,7 +774,7 @@ class ValidateJob(Validator):
         "enabled": True,
         "queueing": True,
         "allow_overlap": False,
-        "max_runtime": datetime.timedelta(hours=168),
+        "max_runtime": datetime.timedelta(days=7),
         "monitoring": {},
         "time_zone": None,
         "expected_runtime": datetime.timedelta(hours=24),
