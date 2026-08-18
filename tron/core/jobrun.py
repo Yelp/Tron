4e19400a1fc822d35dff48123e544e1ad8e49afe
Charles Peacock Arguelles <cpeacock@yelp.com>
2026-08-17 20:21:39 -0700

    snapshot self.runs so get_active() does not suffer from mutation during generator iterations

--- tron/core/jobrun.py
+++ tron/core/jobrun.py
@@ -529,7 +529,7 @@ class JobRunCollection:
 
     def get_active(self, node=None):
         active_states = {ActionRun.RUNNING, ActionRun.STARTING, ActionRun.WAITING}
-        return [r for r in self.runs if (r.state in active_states) and (not node or r.node == node)]
+        return [r for r in list(self.runs) if (r.state in active_states) and (not node or r.node == node)]
 
     def get_first_queued(self, node=None):
         return next_or_none(
