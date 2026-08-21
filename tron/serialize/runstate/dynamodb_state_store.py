18fcd1dfde3465158abc4fba961bd04615603463
Charles Peacock <cmpa8884@gmail.com>
2026-08-21 10:29:40 -0600

    Update max pool connections in DynamoDB config
    Increased max pool connections from 20 to 30.

--- tron/serialize/runstate/dynamodb_state_store.py
+++ tron/serialize/runstate/dynamodb_state_store.py
@@ -51,7 +51,7 @@ class DynamoDBStateStore:
         #
         # It handles transient errors like RequestTimeout and ConnectionError, as well
         # as Service-side errors like Throttling, SlowDown, and LimitExceeded.
-        retry_config = Config(retries={"max_attempts": 5, "mode": "standard"}, max_pool_connections=20)
+        retry_config = Config(retries={"max_attempts": 5, "mode": "standard"}, max_pool_connections=30)
 
         self.dynamodb = boto3.resource("dynamodb", region_name=dynamodb_region, config=retry_config)
         self.client = boto3.client("dynamodb", region_name=dynamodb_region, config=retry_config)
